d823f65ddc306b392e30d35183a6e527d4ba0de2
jarnura <jarnura47@gmail.com>
2017-12-06 15:44:53 +0000

    block-save created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,10 +12,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { BlocksComponent } from './blocks/blocks.component';
 import { BlockService } from './blocks/blocks.service';
 import { BlockSaveComponent } from './block-save/block-save.component';
+import { BlockSaveService } from './block-save/block-save.service';
 
 const appRoutes: Routes = [
     { path: 'district', component: DistrictSaveComponent },
-    { path: 'block', component: BlocksComponent }
+    { path: 'block', component: BlockSaveComponent }
  ];
 
 @NgModule({
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
   providers: [
       DistrictService,
       DistrictSaveService,
-      BlockService
+      BlockService,
+      BlockSaveService
   ],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/block-save/block-save.component.ts
+++ frontend/src/app/block-save/block-save.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Block } from '../blocks/blocks';
+import { BlockSaveService } from './block-save.service';
+import { BlockService } from '../blocks/blocks.service';
 
 @Component({
   selector: 'app-block-save',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockSaveComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private _router: Router,
+      private _dataService: BlockSaveService,
+      private _blockService: BlockService
+      ){} 
 
+
+   blocks: Block[];
+   
   ngOnInit() {
+      this._blockService.getBlocks().subscribe(blocks => { this.blocks = blocks});
   }
+  
+  onCreate(data): void { 
+          this._dataService.createblock(data).subscribe((newdata) => {
+            this.blocks = this.blocks.concat(newdata);
+          });
+       }
 
 }
